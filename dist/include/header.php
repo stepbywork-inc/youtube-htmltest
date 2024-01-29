1ea5e29dd585937bd5cb3f36af30d9db9869cd97
uesugi <h.uesugi@step-by.work>
2024-01-29 19:32:18 +0900

    no message

---
+++ dist/include/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title><?php echo $TITLE; ?></title>
+  <meta name="description" content="<?php echo $DESC; ?>">
+  <link rel="shortcut icon" href="<?php echo $PATH; ?>assets/images/favicon.png">
+  <meta name="format-detection" content="telephone=no">
+  <script src="<?php echo $PATH; ?>assets/js/main.js"></script>
+  <link rel="stylesheet" href="<?php echo $PATH; ?>assets/css/style.css">
+</head>
+
+<body>
